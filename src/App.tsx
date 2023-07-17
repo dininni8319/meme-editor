1c83ca365ee466fcf9c17b94c217b95fcba766d8
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-07-17 13:59:17 +0200

    implementation for the  sidebar and installed fabric js

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { fabric } from 'fabric'
+import Navbar from './components/navbar'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+   useEffect(() => {
+     const canvas = new fabric.Canvas("canvas", {
+       width: 500,
+       height: 500,
+       backgroundColor: "#fff",
+     })  
+   },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='grid md:grid-cols-6'>
+      
+      <Navbar />
+
+      {/* canvas */}
+      <div className="md:col-span-5 md:w-60 assets-container">
+        <div id="icons-container" className="grid grid-cols-2 gap-2">
+
+        </div>
+        <div id="text-container" className="grid grid-cols-1 gap-2">
+
+        </div>
+        <div id="canvas_cont">
+          <canvas id="canvas"></canvas>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
