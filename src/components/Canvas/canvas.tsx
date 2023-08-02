0aae019aa0de4b719617e598efdb2816dd371619
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-08-02 11:27:25 +0200

    found the code causing the issue with inputs

--- src/components/Canvas/canvas.tsx
+++ src/components/Canvas/canvas.tsx
@@ -1,62 +1,72 @@
-import { useState, useEffect, useRef } from 'react'
-import { fabric } from 'fabric'
+import { useEffect } from 'react'
 import useEvent from '@/hooks/useEvent'
+import useIsMobile from '@/hooks/useIsMobile'
+import useCanvas from '@/hooks/useCanvas'
 
 const Canvas = () => {
-  const canvasRef = useRef(null)
-  const { handleDragOver } = useEvent()
+  const { 
+    handleDragOver, 
+    handleDropElement, 
+    handleKeyDown, 
+    handleBounderies 
+  } = useEvent()
+  const isMobile = useIsMobile()
+  const { canvasElementRef, fabricCanvasRef } = useCanvas(isMobile)
 
   useEffect(() => {
-    const canvasEl = canvasRef.current
-    const canvas = new fabric.Canvas(canvasEl, {
-      width: 500,
-      height: 500,
-      backgroundColor: '#fff',
-      hoverCursor: 'pointer',
-      selection: true,
+    const canvas = fabricCanvasRef?.current
+    if (!canvas) return
+    canvas.on('mouse:wheel', function (opt) {
+      const delta = opt.e.deltaY
+      let zoom = canvas.getZoom()
+      zoom *= 0.999 ** delta
+      if (zoom > 20) zoom = 20
+      if (zoom < 0.01) zoom = 0.01
+      canvas.setZoom(zoom)
+      opt.e.preventDefault()
+      opt.e.stopPropagation()
     })
 
-    if (!canvas && canvasEl === null) {
+    canvas.on('object:modified', (event) => handleBounderies(event, canvas))
+
+    window.addEventListener('drop', (event) => handleDropElement(event, canvas))
+
+    window.addEventListener('dragover', (event) => {
+      event.preventDefault()
+    })
+
+    // window.addEventListener('keydown', (event: KeyboardEvent) =>
+    //   handleKeyDown(event, canvas)
+    // )
+  }, [
+    canvasElementRef,
+    fabricCanvasRef,
+    handleKeyDown,
+    handleDragOver,
+    handleDropElement,
+    handleBounderies
+  ])
+
+  const downloadCanvasAsImage = () => {
+    const canvas = fabricCanvasRef.current
+    if (!canvas) {
       return
     }
-
-    // Dragover event to allow drop
-    canvasEl.addEventListener('dragover', (event: DragEvent) => {
-      event.preventDefault()
-    });
-
-    // Drop event
-    canvasEl.addEventListener('drop', (event: DragEvent) => {
-      
-      event.preventDefault()
-      const imageUrl = event.dataTransfer?.getData('text/plain')
-      console.log("🚀 ~ file: canvas.tsx:33 ~ canvasEl.addEventListener ~ imageUrl:", imageUrl)
-
-      if (!event.target || !event.dataTransfer) {
-        return
-      }
-      const reader = new FileReader();
-      reader.onload = function (event) {
-        const imgObj = new Image();
-        if (!imgObj.src) return
-
-        imgObj.src = event.target.result;
-        imgObj.onload = function () {
-          const img = new fabric.Image(imgObj);
-          img.scale(0.2);
-          canvas.centerObject(img);
-          canvas.add(img);
-          canvas.renderAll();
-        };
-      }
-      reader.readAsDataURL(event.dataTransfer.files[0]);
-    });
-
-  }, [])
+    const link = document.createElement('a')
+    link.download = 'image.png'
+    link.href = canvas.toDataURL()
+    link.click()
+  }
 
   return (
     <>
-      <canvas ref={canvasRef}></canvas>
+      <canvas ref={canvasElementRef} onDragOver={handleDragOver}></canvas>
+      <button
+        onClick={downloadCanvasAsImage}
+        className="mt-5 bg-[#22233E] p-3 rounded-md shadow-2xl"
+      >
+        Download Canvas
+      </button>
     </>
   )
 }
