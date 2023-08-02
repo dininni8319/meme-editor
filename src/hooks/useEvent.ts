3afa8307f2477304e5c5394e97a53c17715420be
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-08-02 11:42:55 +0200

    Update useEvent.ts

--- src/hooks/useEvent.ts
+++ src/hooks/useEvent.ts
@@ -163,29 +163,6 @@ const useEvent = () => {
     }
   }
 
-    const handleBounderies = (event: fabric.IEvent, canvas: fabric.Canvas) => {
-      const obj = event.target as fabric.Object
-      if (!obj) return
-      const rect = obj.getBoundingRect()
-      if (!rect) return
-
-      if (
-        rect.left < 0 ||
-        rect.top < 0 ||
-        rect.left + rect.width > canvas.getWidth() ||
-        rect.top + rect.height > canvas.getHeight()
-      ) {
-        if (obj.getAngle() != obj.originalState.angle) {
-          obj.setAngle(obj.originalState.angle)
-        } else {
-          obj.setTop(obj.originalState.top)
-          obj.setLeft(obj.originalState.left)
-          obj.setScaleX(obj.originalState.scaleX)
-          obj.setScaleY(obj.originalState.scaleY)
-        }
-        obj.setCoords()
-      }
-    }
   return {
     handleDragStart,
     handleDragOver,
